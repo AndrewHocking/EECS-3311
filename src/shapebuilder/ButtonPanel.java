427d4bbd51987e9339e6e09e69124f859f2d106a
AndrewHocking <andrew.hocking@hotmail.ca>
2021-09-29 12:56:49 -0400

    Implemented bubble sort technique and tidied up some code

--- src/shapebuilder/ButtonPanel.java
+++ src/shapebuilder/ButtonPanel.java
@@ -11,21 +11,29 @@ public class ButtonPanel extends JPanel implements ActionListener {
 	
 	private static final long serialVersionUID = -5412098210805709542L;
 	
+	private final class ButtonAction {
+		public static final String LOAD_SHAPES = "Load shapes";
+		public static final String SORT_SHAPES = "Sort shapes";
+	}
+	
+	private JButton loadButton;
+	private JButton sortButton;
+
 	private ShapePanel shapePanel;
-	private JButton loadButton = new JButton("Load shapes");
-	private JButton sortButton = new JButton("Sort shapes");
 
 	public ButtonPanel(ShapePanel shapePanel) {
 		super(new FlowLayout());
 		
 		// Add "Load shapes" button
+		loadButton = new JButton(ButtonAction.LOAD_SHAPES);
 		loadButton.addActionListener(this);
-		loadButton.setActionCommand("load shapes");
+		loadButton.setActionCommand(ButtonAction.LOAD_SHAPES);
 		this.add(loadButton);
 		
-		// Add "Sort shapes" button
+		// Add "Sort shapes" button, disabled by default
+		sortButton = new JButton(ButtonAction.SORT_SHAPES);
 		sortButton.addActionListener(this);
-		sortButton.setActionCommand("sort shapes");
+		sortButton.setActionCommand(ButtonAction.SORT_SHAPES);
 		sortButton.setEnabled(false);
 		this.add(sortButton);
 		
@@ -35,12 +43,12 @@ public class ButtonPanel extends JPanel implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		switch (e.getActionCommand()) {
-			case "load shapes":
+			case ButtonAction.LOAD_SHAPES:
 				// Generate the shapes, repaint the screen, and enable the sort button
 				ShapeFactory.generateShapes(shapePanel);
 				sortButton.setEnabled(true);
 				break;
-			case "sort shapes":
+			case ButtonAction.SORT_SHAPES:
 				// Sort the shapes by surface area, repaint the screen, and disable the sort button
 				SortingTechnique.sortBySurfaceArea(shapePanel);
 				sortButton.setEnabled(false);
--- src/shapebuilder/SortingTechnique.java
+++ src/shapebuilder/SortingTechnique.java
@@ -1,23 +1,37 @@
 package shapebuilder;
 
-import java.util.Comparator;
+import java.util.List;
 
 public class SortingTechnique {
 	
+	/**
+	 * Sorts shapes by their surface area in ascending order using Bubble Sort, then repaints the screen.
+	 * @param shapePanel
+	 */
 	public static void sortBySurfaceArea(ShapePanel shapePanel) {
-		shapePanel.getShapes().sort(Comparator.naturalOrder());
-		
-		int x = 10;
-		int y = 10;
-		
+		List<Shape> shapes = shapePanel.getShapes();
+        int numberOfShapes = shapes.size();
+        for (int i = 0; i < numberOfShapes - 1; i++) {
+            for (int j = 0; j < numberOfShapes - i - 1; j++) {
+                if (shapes.get(j).compareTo(shapes.get(j + 1)) > 0) {
+                    Shape temp = shapes.get(j);
+                    shapes.set(j, shapes.get(j + 1));
+                    shapes.set(j + 1, temp);
+                }
+            }
+        }
+        repaintShapes(shapePanel);
+    }
+	
+	private static void repaintShapes(ShapePanel shapePanel) {
+		int x = ShapeFactory.SPACING;
+		int y = ShapeFactory.SPACING;
 		for (Shape shape : shapePanel.getShapes()) {
 			shape.setX(x);
 			shape.setY(y);
 			x += shape.getWidth() + ShapeFactory.SPACING;
 			y += shape.getHeight() + ShapeFactory.SPACING;
 		}
-		
 		shapePanel.repaint();
 	}
-
 }
